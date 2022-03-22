674fd7f4ab66f4931410831dc3ee466d0e63c23f
enygf <gg.wp.0004.18@gmail.com>
2022-03-22 14:30:33 +0300

    arithmeticSequanceSum is done

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import sumAverage from "./components/arr-functions/sumAverage/sumAverage";
 import maxTripletSum from "./components/arr-functions/maxTripletSum/maxTripletSum";
 import biggest from "./components/arr-functions/biggest/biggest";
 import pattern from "./components/arr-functions/pattern/pattern";
+import arithmeticSequenceSum from "./components/arr-functions/arithmeticSequenceSum/aritmeticSequenceSum";
 
 console.log(sumAverage([1, 2, 3], [5, 1, 2], [9, 0, 0]));
 console.log(maxTripletSum([1, 2, 3, 5, 5]));
 console.log(biggest([92, 0, 56, 1, 83]));
 console.log(pattern(5));
+console.log(arithmeticSequenceSum(2, 3, 5));
