671b764b3a4ded61875c05202b69c12a5e0ab187
enygf <gg.wp.0004.18@gmail.com>
2022-03-22 13:11:16 +0300

    mid sum of arrays is done

---
+++ src/components/arr-functions/midSum/midSum.js
@@ -0,0 +1,14 @@
+const midSum = (...arr) => {
+	let value = 0;
+	let count = 0;
+	for (let el of arr) {
+		for (let val of el) {
+			value += val;
+			count++;
+		}
+	}
+
+	return Math.floor(value / count);
+};
+
+export default midSum;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles/main.sass";
+import midSum from "./components/arr-functions/midSum/midSum";
+
+console.log(midSum([1, 2, 3], [5, 1, 2], [9, 0, 0]));
