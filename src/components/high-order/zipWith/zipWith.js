0802f7198b041fc12669d1afc4a5079342f85093
enygf <gg.wp.0004.18@gmail.com>
2022-03-25 13:44:30 +0300

    rewrote zipWith with slice + map

--- src/components/high-order/zipWith/zipWith.js
+++ src/components/high-order/zipWith/zipWith.js
@@ -1,12 +1,7 @@
 const zipWith = (func, arr1, arr2) => {
-  let newArr = [];
-
-  arr1.forEach((item, index) => {
-    let value = func(item, arr2[index]);
-    newArr.push(value);
-  });
-
-  return newArr;
+  return arr1.length > arr2.length
+    ? arr1.slice(0, arr2.length).map((item, index) => func(item, arr2[index]))
+    : arr1.map((item, index) => func(item, arr2[index]));
 };
 
 export default zipWith;
