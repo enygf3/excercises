7a8ff942e2cec1c1bfaab559ee4cb8bfddc4c4df
enygf <gg.wp.0004.18@gmail.com>
2022-03-25 14:45:59 +0300

    rewrote romanToNumber with map + reduce

--- src/components/string-functions/romanToNumber/romanToNumber.js
+++ src/components/string-functions/romanToNumber/romanToNumber.js
@@ -1,15 +1,10 @@
 const romanToNumber = (number) => {
   let data = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
-  let value = 0;
-  let romanValue = number.split("").forEach((item, index, arr) => {
-    if (data[item] > data[arr[index]]) {
-      value += data[item] - arr[index];
-    } else {
-      value += data[item];
-    }
-  });
-
-  return value;
+  return number.replace(/[MDCLXVI]/g, "").length == 0
+    ? Array.from(number)
+        .map((item) => data[item])
+        .reduce((prev, item) => (prev >= item ? prev + item : item - prev), 0)
+    : 0;
 };
 
 export default romanToNumber;
