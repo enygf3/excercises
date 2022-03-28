a9602de1f0b50792f3b0364d1ee51f6b3eccc879
enygf <gg.wp.0004.18@gmail.com>
2022-03-28 10:50:06 +0300

    minified code in pattern

--- src/components/arr-functions/pattern/pattern.js
+++ src/components/arr-functions/pattern/pattern.js
@@ -1,15 +1,11 @@
 const pattern = (num) => {
-  let newArr = [];
-  Array.from(Array(num).keys())
+  return Array.from(Array(num).keys())
     .map((x) => (++x).toString())
-    .forEach((item, index, arr) => {
-      let str = "";
-      str =
-        arr.join("").substring(index, num) + arr.join("").substring(0, index);
-      newArr.push(str);
-    });
-
-  return newArr;
+    .map(
+      (item, index, arr) =>
+        (item =
+          arr.join("").substring(index, num) + arr.join("").substring(0, index))
+    );
 };
 
 export default pattern;
