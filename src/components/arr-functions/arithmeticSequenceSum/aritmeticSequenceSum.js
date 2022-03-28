c93c1213bc3d436dc0dca5fc5b3198a4c208fbe4
enygf <gg.wp.0004.18@gmail.com>
2022-03-28 12:16:06 +0300

    rewrote arithmeticSequenceSum with methods

--- src/components/arr-functions/arithmeticSequenceSum/aritmeticSequenceSum.js
+++ src/components/arr-functions/arithmeticSequenceSum/aritmeticSequenceSum.js
@@ -1,15 +1,5 @@
 const arithmeticSequenceSum = (a, r, n) => {
-  let value = 0;
-
-  for (let i = 0; i < n; i++) {
-    value += a;
-  }
-
-  for (let i = 1; i <= n - 1; i++) {
-    value += r * i;
-  }
-
-  return value;
+  return Array.from(Array(n).keys()).map((item) => a + item * r);
 };
 
 export default arithmeticSequenceSum;
