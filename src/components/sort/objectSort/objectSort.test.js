a26b8427cc3957eb9717d7373cb33f245bf5f317
enygf <gg.wp.0004.18@gmail.com>
2022-04-01 19:15:55 +0300

    added additional test to objectSort for 100% coverage

--- src/components/sort/objectSort/objectSort.test.js
+++ src/components/sort/objectSort/objectSort.test.js
@@ -8,6 +8,14 @@ test("{ a: 1 }, { a: 2 }, { a: 3 } to be equal {a: 3}, {a: 2}, {a: 1}", () => {
   ]);
 });
 
+test("{ a: 2 }, { a: 2 }, { a: 3 } to be equal {a: 3}, {a: 2}, {a: 2}", () => {
+  expect(objectSort([{ a: 2 }, { a: 2 }, { a: 3 }])).toStrictEqual([
+    { a: 3 },
+    { a: 2 },
+    { a: 2 },
+  ]);
+});
+
 test("{ a: 87 }, { a: -2 }, { a: 43 }, { a: 0} to be equal {a: 87}, {a: 43}, {a: 0}, {a: -2}", () => {
   expect(objectSort([{ a: 87 }, { a: -2 }, { a: 43 }, { a: 0 }])).toStrictEqual(
     [{ a: 87 }, { a: 43 }, { a: 0 }, { a: -2 }]
